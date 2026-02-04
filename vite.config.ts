53e3ab1a96f88214d603c2d05d969a848e802ca2
Alaa El-nakeeb <103367236+AlaaElnakeeb81536@users.noreply.github.com>
2026-02-04 17:32:48 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // غيّريه لـ '/اسم-الريبو/' لو الريبو مش username.github.io
+  server: {
+    port: 5173
+  }
+})
